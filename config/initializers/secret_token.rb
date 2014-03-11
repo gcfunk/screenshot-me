b2cc7248378f419a2aff67055cd2aa84c09398df
Greg Funk <greg.funk@walltowall.com>
2014-03-11 09:59:17 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScreenshotMe::Application.config.secret_token = '73fe184935e48099b1a512a5f9aa903cee602310a6f3e665a3446d773c935bbdb68bacd081d244605454135824dcf465751ff8ce93094d463c7608eebf397e4b'
